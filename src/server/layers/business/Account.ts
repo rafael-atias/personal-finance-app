8472a3725d55362dd2621a213de273a1ddc93b00
Rafael Atías <22062169+rafael-atias@users.noreply.github.com>
2023-09-27 06:00:42 -0300

    Created class Account file

---
+++ src/server/layers/business/Account.ts
@@ -0,0 +1,9 @@
+import { Currency } from "./Currency";
+import { Money } from "./Money";
+import { Transaction, TransactionType } from "./Transaction";
+
+export class Account {
+    constructor() {
+        
+    }
+}
